2e5b794c3d23b334e8703e7e4e0540975b95991f
LiamDonohue <liamd@naturallygrownkids.org>
2020-12-18 12:54:32 +0000

    g

--- main.cs
+++ main.cs
@@ -3,53 +3,42 @@ using System.Threading;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
+using System.Diagnostics;
 
 public class Transaction
 {
    public int Id { get; set; }
    public string Sender { set; get; }
    public string Recipient { set; get; }
-   public decimal Amount { set; get; }
-   public decimal Fee { set; get; }
-}
-
-public class Block
-{
-    public int Height { get; set; }
-    public Int64 TimeStamp { get; set; }
-    public byte[] PrevHash { get; set; }
-    public byte[] Hash { get; set; }
-    public Transaction[] Transactions { get; set; }
-    public string Creator { get; set; }
-}
-  
-
-  public byte[] GenerateHash()
-{
-    var sha = SHA256.Create();
-    
-    //convert current timestamp to array of byte
-    byte[] timeStamp = BitConverter.GetBytes(TimeStamp);
-
-    //convert transaction collection to array of byte
-    var transactionHash = Transactions.ConvertToByte();
-
-    //make block header in array of byte
-    byte[] headerBytes = new byte[timeStamp.Length + PrevHash.Length + transactionHash.Length];
-
-    Buffer.BlockCopy(timeStamp, 0, headerBytes, 0, timeStamp.Length);
-    Buffer.BlockCopy(PrevHash, 0, headerBytes, timeStamp.Length, PrevHash.Length);
-    Buffer.BlockCopy(transactionHash, 0, headerBytes, timeStamp.Length + PrevHash.Length, transactionHash.Length);
-
-    byte[] hash = sha.ComputeHash(headerBytes);
-
-    return hash;
-
+   public double Amount { set; get; }
+   public double Fee { set; get; }
 }
 
 class main{
+
   static void Main(){
-  
+  //Example transaction data
+var trx1 = new Transaction()
+   {
+      Sender = "Ricard",
+      Recipient = "Frodo",
+      Amount = 10,
+      Fee = 0.001
+   };
+   
+
+//example transaction data   
+var trx2 = new Transaction()
+   {
+      Sender = "Amir",
+      Recipient = "Wati",
+      Amount = 20,
+      Fee = 0.0001
+   };   
+   Console.WriteLine(trx2.Sender);
+   Console.Read();
+   
 
   }
 }
